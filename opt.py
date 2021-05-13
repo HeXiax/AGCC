d917c82527f9e81c5e9cbd623139dca3cc6ff3fb
HeXiax <83396811+HeXiax@users.noreply.github.com>
2021-05-13 08:59:13 +0800

    Add files via upload

---
+++ opt.py
@@ -0,0 +1,21 @@
+import argparse
+parser = argparse.ArgumentParser(description='train', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--name', type=str, default='reut')
+parser.add_argument('--k', type=int, default=3)
+parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--n_clusters', default=3, type=int)
+parser.add_argument('--n_z', default=10, type=int)
+parser.add_argument('--pretrain_path', type=str, default='pkl')
+parser.add_argument('--seed', type=int, default=2)
+parser.add_argument('--seed_dblp', type=int, default=14)
+parser.add_argument('--seed_usps', type=int, default=5)
+parser.add_argument('--seed_cite', type=int, default=4)
+parser.add_argument('--seed_acm', type=int, default=10)
+parser.add_argument('--seed_hhar', type=int, default=11)
+parser.add_argument('--seed_reut', type=int, default=18)
+parser.add_argument('--lambda_v1', type=float,default=0.1)
+parser.add_argument('--lambda_v2', type=float, default=0.01)
+parser.add_argument('--lambda_v3', type=float, default=0.01)
+
+args = parser.parse_args()
